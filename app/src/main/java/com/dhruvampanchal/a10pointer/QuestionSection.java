c88a737a1776af1e8f5b12aaa9db9565f725c602
Dhruvam Panchal <dhruvampanchal@gmail.com>
2018-10-06 23:06:31 +0530

    Latest yet(2).

--- app/src/main/java/com/dhruvampanchal/a10pointer/QuestionSection.java
+++ app/src/main/java/com/dhruvampanchal/a10pointer/QuestionSection.java
@@ -8,8 +8,10 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -23,18 +25,16 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Logger;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class QuestionSection extends AppCompatActivity {
 
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+
     private ArrayList<String> myDataset = new ArrayList<String>();
     private DatabaseReference myRef;
-    private ArrayList<String> mUsername = new ArrayList<String>();
-    private ArrayList<String> mQuestions = new ArrayList<String>();
     private ListView QuestionsView;
     private String Question;
     @Override
@@ -46,21 +46,23 @@ public class QuestionSection extends AppCompatActivity {
 
         myRef = FirebaseDatabase.getInstance().getReference().child("subjects").child("physics");
 
+        myDataset.clear();
+
         myRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                int i = 0;
+                int x = 0;
                 Toast.makeText(QuestionSection.this, dataSnapshot.getKey().toString(), Toast.LENGTH_SHORT).show();
                 for (DataSnapshot child: dataSnapshot.getChildren()) {
                     Toast.makeText(QuestionSection.this, "Inside Child!", Toast.LENGTH_SHORT).show();
                     Log.i("CHILD ACTIVITY", child.getKey().toString());
 
-
-                    myDataset.add(child.getValue().toString());
+                    //Adding to myDataset
+                    myDataset.add(x,child.getValue().toString());
                     Log.i("CHILD Value", child.getValue().toString());
                     Toast.makeText(QuestionSection.this, "Question Added!", Toast.LENGTH_SHORT).show();
-                    Log.i("myDataSet",myDataset.get(i).toString());
-                    i = i+1;
+                    Log.i("myDataSet",myDataset.get(x).toString());
+                    x=x+1;
                 }
             }
 
@@ -85,28 +87,41 @@ public class QuestionSection extends AppCompatActivity {
             }
         });
 
+
         QuestionsView = (ListView) findViewById(R.id.QList);
-        final ArrayAdapter arrayAdapter = new ArrayAdapter<String>(
-                this,R.layout.activity_question_section, myDataset);
-        QuestionsView.setAdapter(arrayAdapter);
 
-        /*
-        mRecyclerView = (RecyclerView) findViewById(R.id.QuestionsView);
-
-        //Setting fixed size of Recycler View
-        mRecyclerView.setHasFixedSize(true);
-
-        //use a linear layout manager
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        //specify an Adapter
-        mAdapter = new MyAdapter(myDataset);
-        mRecyclerView.setAdapter(mAdapter);
-        */
+        CustomAdapter customAdapter = new CustomAdapter();
+        QuestionsView.setAdapter(customAdapter);
 
 
 
+    }
+
+    class CustomAdapter extends BaseAdapter{
+        @Override
+        public int getCount() {
+            return myDataset.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = getLayoutInflater().inflate(R.layout.listitem, null);
+
+            TextView Questions = (TextView) convertView.findViewById(R.id.TVText);
+            Questions.setText(myDataset.get(position));
+
+            return convertView;
+        }
     }
 
 }
